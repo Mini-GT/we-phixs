9cb66b39e390af21d85a732a6d2ce78de615703a
MiniGT <bendanillo539@gmail.com>
2025-10-03 19:33:30 +0800

    chore: delete unused resource

--- apps/web/api/user.ts
+++
@@ -1,6 +0,0 @@
-import { type User } from "@repo/types"
-
-export const getUsers = async (): Promise<User[]> => {
-  const user = await fetch(`${process.env.API_URL}/users`)
-  return user.json()
-}
