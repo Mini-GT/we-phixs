1f48e8abdfc0ca03ca056e4814211886e1f74e46
MiniGT <bendanillo539@gmail.com>
2025-10-03 19:38:25 +0800

    refactor: turn client component into server

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,40 +1,14 @@
-"use client"
-
-import { useState } from "react";
-// import Image, { type ImageProps } from "next/image";
+import { cookies } from "next/headers";
 import Canvas from "./components/canvas";
-import Login from "./components/login";
-import LoginForm from "./components/form/loginForm";
-import CardModal from "./components/cardModal";
-import { useComponent } from "./context/component.context";
-import RegisterForm from "./components/form/registerForm";
+import Login from "./components/loginBtn";
+import User from "./components/user";
 
-// type Props = Omit<ImageProps, "src"> & {
-//   srcLight: string;
-//   srcDark: string;
-// };
-
-// const ThemeImage = (props: Props) => {
-//   const { srcLight, srcDark, ...rest } = props;
-
-//   return (
-//     <>
-//       <Image {...rest} src={srcLight} className="imgLight" />
-//       <Image {...rest} src={srcDark} className="imgDark" />
-//     </>
-//   );
-// };
-
-export default function Home() {
-  const { component, setComponent } = useComponent()
+export default async function Home() {
+  const token = (await cookies()).get("token")?.value;
 
   return (
     <div className="flex flex-col items-center border-black w-full">
-      {component === "loginForm" && <LoginForm setComponent={setComponent} />}
-      {component === "registerForm" && <RegisterForm setComponent={setComponent} />}
-      <Canvas>
-        <Login setComponent={setComponent} />
-      </Canvas>
+      <Canvas>{!token ? <Login /> : <User />}</Canvas>
     </div>
   );
 }
