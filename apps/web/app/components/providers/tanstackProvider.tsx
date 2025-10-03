5a4c934b6e83ce61fd3bc3445d7e9114478f8776
MiniGT <bendanillo539@gmail.com>
2025-10-03 13:14:11 +0800

    feat: enhance tanstack provider

--- apps/web/app/components/providers/tanstackProvider.tsx
+++ apps/web/app/components/providers/tanstackProvider.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode, useState } from "react";
+import { getQueryClient } from "@/getQueryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
 
-export default function TanstackProvider({ children }: { children: ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+export default function TanstackProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const queryClient = getQueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
