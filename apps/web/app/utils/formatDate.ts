0d3234ce5a384314347b456136fa36a445d50000
MiniGT <bendanillo539@gmail.com>
2025-10-04 16:52:41 +0800

    feat: add formate date utility

---
+++ apps/web/app/utils/formatDate.ts
@@ -0,0 +1,18 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
+
+const toReadableDate = (isoString: Date | string): string => {
+  const date = new Date(isoString);
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const day = String(date.getUTCDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
+
+const convertToTimeAgo = (timestamp: Date) => {
+  return dayjs(timestamp).fromNow();
+};
+
+export { toReadableDate, convertToTimeAgo };
