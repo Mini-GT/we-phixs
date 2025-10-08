b7cec84fe7b6ab32618d5715d19cf25d0037b503
MiniGT <bendanillo539@gmail.com>
2025-10-08 18:12:23 +0800

    feat: add type and disabled props to IconButton component

--- apps/web/app/components/ui/iconButton.tsx
+++ apps/web/app/components/ui/iconButton.tsx
@@ -5,15 +5,25 @@ interface IconButtonProps {
   children: React.ReactNode;
   onClick?: () => void;
   className?: string;
+  type?: "submit" | "button";
+  disabled?: boolean;
 }
 
-export default function IconButton({ children, onClick, className }: IconButtonProps) {
+export default function IconButton({
+  children,
+  onClick,
+  className,
+  disabled = false,
+  type = "button",
+}: IconButtonProps) {
   const baseStyles =
     "flex items-center justify-center shadow-md border-3 border-gray-200 hover:bg-gray-200 hover:scale-90 active:scale-100 transition cursor-pointer";
 
   return (
     <button
+      type={type}
       onClick={onClick}
+      disabled={disabled}
       className={clsx("w-10 h-10 rounded-full bg-white", baseStyles, className)}
     >
       {children}
