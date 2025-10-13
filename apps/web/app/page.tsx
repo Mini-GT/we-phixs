501ff1157dce3d0092a8a2c8c88522e10eda54ac
MiniGT <bendanillo539@gmail.com>
2025-10-13 09:40:58 +0800

    refactor: prefetching logic and improve component structure in Home

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,14 +3,15 @@ import Canvas from "./components/canvas";
 import Login from "./components/loginBtn";
 import User from "./components/user";
 import CardContent from "./components/cardContent";
-import { Slide, toast, ToastContainer } from "react-toastify";
+import { Slide, ToastContainer } from "react-toastify";
 import { getCanvasById } from "api/canvas.service";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { queryKeysType } from "@repo/types";
 import { getQueryClient } from "./getQueryClient";
 import { getPaintCharges } from "api/user.service";
 import { isTokenExpired, verifyJwt } from "./utils/jwt";
-import { logoutUser } from "api/auth.service";
+import { getAllLeaderboard } from "api/leaderboard.service";
+import { logoutUser } from "api/logout.service";
 
 export const jwtsecret = process.env.jwtsecretKey;
 
@@ -27,31 +28,36 @@ export default async function Home() {
     if (isExpired) {
       await logoutUser();
     } else {
-      await queryClient.prefetchQuery({
-        queryKey: queryKeysType.paintCharges,
-        queryFn: () => getPaintCharges(id),
-      });
+      await Promise.all([
+        await queryClient.prefetchQuery({
+          queryKey: queryKeysType.canvas(1),
+          queryFn: async () => await getCanvasById(1),
+        }),
+        await queryClient.prefetchQuery({
+          queryKey: queryKeysType.allLeaderboard,
+          queryFn: async () => await getAllLeaderboard(),
+        }),
+        await queryClient.prefetchQuery({
+          queryKey: queryKeysType.paintCharges,
+          queryFn: () => getPaintCharges(id),
+        }),
+      ]);
     }
   }
 
-  await queryClient.prefetchQuery({
-    queryKey: queryKeysType.canvas(1),
-    queryFn: async () => await getCanvasById(1),
-  });
-
   return (
-    <div className="flex flex-col items-center border-black w-full">
-      <ToastContainer
-        autoClose={3000}
-        pauseOnHover={false}
-        position="top-left"
-        transition={Slide}
-        stacked
-      />
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Canvas>{!hasLoginToken ? <Login /> : <User />}</Canvas>
-      </HydrationBoundary>
-      <CardContent />
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex flex-col items-center border-black w-full">
+        <ToastContainer
+          autoClose={3000}
+          pauseOnHover={false}
+          position="top-left"
+          transition={Slide}
+          stacked
+        />
+        <Canvas hasLoginToken={hasLoginToken}>{!hasLoginToken ? <Login /> : <User />}</Canvas>
+        <CardContent />
+      </div>
+    </HydrationBoundary>
   );
 }
