95af2912c7601b3b153a090fd54881a0c105f276
MiniGT <bendanillo539@gmail.com>
2025-11-06 21:33:07 +0800

    feat: enhance redirect logic based on login status in guild join route

--- apps/web/app/guild/join/route.ts
+++ apps/web/app/guild/join/route.ts
@@ -1,10 +1,18 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const code = searchParams.get("code");
+  const cookieStore = await cookies();
+  const loginToken = cookieStore.get("loginToken")?.value;
 
-  const res = NextResponse.redirect(new URL("/login", request.url));
+  const code = searchParams.get("code");
+  let res;
+  if (loginToken) {
+    res = NextResponse.redirect(new URL("/", request.url));
+  } else {
+    res = NextResponse.redirect(new URL("/login", request.url));
+  }
 
   // store cookie to be reuse once user has logged in
   if (code) res.cookies.set("guildInvite", code, { path: "/", httpOnly: true, secure: true });
