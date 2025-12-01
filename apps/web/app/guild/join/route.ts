672157f33b46228908a00996e7ed04c7fa768ddc
Jill Ared <bendanillo539@gmail.com>
2025-12-01 09:30:36 +0800

    refactor: use public domain for redirection instead of using the request url

--- apps/web/app/guild/join/route.ts
+++ apps/web/app/guild/join/route.ts
@@ -1,21 +1,28 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
+const PUBLIC_DOMAIN = process.env.NEXT_PUBLIC_CLIENT_URL;
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const cookieStore = await cookies();
   const loginToken = cookieStore.get("loginToken")?.value;
 
   const code = searchParams.get("code");
-  let res;
-  if (loginToken) {
-    res = NextResponse.redirect(new URL("/", request.url));
-  } else {
-    res = NextResponse.redirect(new URL("/login", request.url));
-  }
+  let redirectPath = loginToken ? "/" : "/login";
+
+  const destinationUrl = new URL(redirectPath, PUBLIC_DOMAIN);
+
+  // 2. Perform the redirect
+  const res = NextResponse.redirect(destinationUrl);
 
   // store cookie to be reuse once user has logged in
-  if (code) res.cookies.set("guildInvite", code, { path: "/", httpOnly: true, secure: true });
+  if (code)
+    res.cookies.set("guildInvite", code, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+    });
 
   return res;
 }
