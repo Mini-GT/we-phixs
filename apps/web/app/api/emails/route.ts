be6cae12d1372a99f11548e29081e6eb5a0f8666
Jill Ared <bendanillo539@gmail.com>
2025-12-08 14:54:44 +0800

    feat: use nodemailer as we are now using our own mail server mailcow:dockerize

--- apps/web/app/api/emails/route.ts
+++ apps/web/app/api/emails/route.ts
@@ -3,12 +3,27 @@ import { forgotPassword } from "api/email.service";
 import { isAxiosError } from "axios";
 import { NextRequest } from "next/server";
 import { Resend } from "resend";
+import { render } from "@react-email/components";
+import nodemailer from "nodemailer";
 
 const resend = new Resend(process.env.resendKey);
 
 export async function POST(req: NextRequest) {
-  const email = (await req.json()).email;
   try {
+    const email = (await req.json()).email;
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.TRANSPORT_HOST,
+      // port: 587,
+      port: 465,
+      // secure: false, // true for 465, false for other ports
+      secure: true,
+      auth: {
+        user: process.env.TRANSPORT_EMAIL_USER,
+        pass: process.env.TRANSPORT_EMAIL_PASSWORD,
+      },
+    });
+
     const result = await forgotPassword(email);
     const { username, token } = result;
     const clientUrl = process.env.NEXT_PUBLIC_CLIENT_URL;
@@ -17,12 +32,21 @@ export async function POST(req: NextRequest) {
       token
     )}&email=${encodeURIComponent(email)}`;
 
-    await resend.emails.send({
-      from: "Phixel Paint <no-reply@phixelpaint.com>",
+    const emailHtml = await render(ResetPasswordEmail({ username, resetLink }));
+
+    await transporter.sendMail({
+      from: `${process.env.TRANSPORT_FROM_NAME} <${process.env.TRANSPORT_FROM_EMAIL}>`,
       to: email,
       subject: "Password Reset",
-      react: ResetPasswordEmail({ username, resetLink }),
+      html: emailHtml,
     });
+
+    // await resend.emails.send({
+    //   from: "Phixel Paint <no-reply@phixelpaint.com>",
+    //   to: email,
+    //   subject: "Password Reset",
+    //   react: ResetPasswordEmail({ username, resetLink }),
+    // });
 
     return Response.json(
       {
